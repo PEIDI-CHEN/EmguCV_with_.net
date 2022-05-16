43025ffc0e2be7218ed367a45fd8cf5340102dd0
PEIDI-CHEN <91669751+PEIDI-CHEN@users.noreply.github.com>
2022-05-16 04:30:02 +0000

    Add files via upload

---
+++ WPF_facialrecognition/WPF_facialrecognition/Path.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_facialrecognition
+{
+    public static class Path
+    {
+        public static string FacePhotosPath = "Source\\Faces\\";
+        public static string FaceListTextFile = "Source\\FaceList.txt";
+        public static string HaarCascadePath = "Resources\\haarcascade_frontalface_default.xml";
+        public static int TimerResponseValue = 500;
+        public static string ImageFileExtension = ".bmp";
+        public static int ActiveCameraIndex = 0;//0: Default active camera device
+    }
+}
