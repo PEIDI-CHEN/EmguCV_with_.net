447a8e24cfb4cb11aca48583772a0123df332ce9
PEIDI-CHEN <91669751+PEIDI-CHEN@users.noreply.github.com>
2022-05-16 04:28:24 +0000

    Add files via upload

---
+++ WPF_facialrecognition/WPF_facialrecognition/Model/MainWindowModel.cs
@@ -0,0 +1,20 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace WPF_facialrecognition.Model
+{
+    public class MainWindowModel
+    {
+        public string? PersonName { get; set; }
+        public Image<Gray, byte>? FaceImage { get; set; }
+        public DateTime CreateDate { get; set; }
+        public Timer captureTimer { get; set; }
+
+    }
+}
